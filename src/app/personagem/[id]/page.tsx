3ebfd6adad96764c89f44c2fe7f147e0080aad24
TiagoBpz <tiagobrasileiroplt@gmail.com>
2025-05-12 15:09:21 -0300

    Add files via upload

---
+++ src/app/personagem/[id]/page.tsx
@@ -0,0 +1,50 @@
+
+
+interface IPersonagem{
+    params:{
+        id:string
+    }
+}
+
+interface IData{
+    name:string;
+    status:string;
+    species:string;
+    gender: string;
+    id: string;
+}
+
+interface IDataStaticIndex {
+    results:{
+        id:number;
+    }[]
+}
+
+
+
+
+const Personagem = async ({params: {id}} : IPersonagem) => {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {cache: 'no-store'})
+    const data: IData = await res.json()
+
+
+    return(
+        <div>
+            <h1 className="text-9xl">{data.id}</h1>
+            <p>{data.species}</p>
+            <p>{data.name}</p>
+            <p>{data.gender}</p>
+        </div>
+    )
+}
+
+export default Personagem;
+
+export async function GenerateStaticParams(){
+    const res = await fetch('https://rickandmortyapi.com/api/character')
+    const data: IDataStaticIndex = await res.json()
+
+    return data.results.map((item) => ({
+        id: item.id.toString()
+    }))
+}
